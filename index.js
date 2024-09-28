29f2da738ed1434ef05136db36100d41cd0221f2
Fatima Manzoor <fatimamnzkayani@gmail.com>
2024-09-28 14:05:19 +0500

    basic functuality of sunset & sunrise App

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+const message = "Hello World";
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {message : message})
+})
+
+app.post("/post-data", async (req, res) => {
+    try {
+        const long = req.body['longitude'];
+        const lat = req.body['latitude'];
+        const date = req.body['date'];
+      const result = await axios.get(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${long}&date=${date}`);
+    //   const resultObject = JSON.stringify(result.data.results);
+      res.render("index.ejs", {
+        // content: JSON.stringify(result.data.results),
+        sunrise : JSON.stringify(result.data.results.sunrise),
+        sunset : JSON.stringify(result.data.results.sunset),
+        message: message,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500);
+    }
+  });
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
